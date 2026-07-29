576c7476a664e491bc49e4b2efa750cdb4bf6ba9
Roheet <roheetbasu@gmail.com>
2026-07-29 18:37:43 +0545

    completed load single function

--- SRC/Training_data_bot/sources/unified.py
+++ SRC/Training_data_bot/sources/unified.py
@@ -12,7 +12,7 @@ from .documents import DocumentLoader
 from .pdf import PDFLoader
 from .web import WebLoader
 from ..core.models import Document, DocumentType
-from ..core.exceptions import DocumentLoadError, UnsupportedFormat
+from ..core.exceptions import DocumentLoadError, UnsupportedFormatError
 from ..core.logging import get_logger, LogContext
 
 class UnifiedLoader(BaseLoader):
@@ -39,6 +39,38 @@ class UnifiedLoader(BaseLoader):
                 
                 # All supported formats
                 self.supported_formats = list(DocumentLoader)
-                
+           
+        async def load_single(
+                self,
+                source: Union[str, Path],
+                **kwargs
+        ) -> Document:
+                """
+                Loads document 
+                """
+                with LogContext("Unified_load_single", source=str(source)):
+                        try:
+                                #Determine source type and select appropriate leader
+                                loader = self.__select__loader(source)
+                                
+                                if loader is None:
+                                        raise UnsupportedFormatError(
+                                                file_format=str(source),
+                                                supported_formats=[fmt.value for fmt in self.supported_formats]
+                                        )
+
+                                #Load using selected loader
+                                document = await loader.load_single(source, **kwargs)
+                                self.logger.debug(f"Successfully loaded {source} using {loader.__class__.__name__}")
+                                
+                                return document
+                        
+                        except Exception as e:
+                                raise DocumentLoadError(
+                                        f"failed to load document from {source}",
+                                        file_path=str(source),
+                                        cause=e
+                                )
+                 
                 
         
