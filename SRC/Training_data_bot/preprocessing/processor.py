4811ec172cd61d37c3c0ff9b8aa913adbe09fa5e
Roheet <roheetbasu@gmail.com>
2026-08-05 17:55:52 +0545

    debugged the errors

--- SRC/Training_data_bot/preprocessing/processor.py
+++ SRC/Training_data_bot/preprocessing/processor.py
@@ -1,4 +1,6 @@
 import re
+from typing import List
+
 from ..core.models import Document, TextChunk
 from ..core.config import settings
 from ..core.logging import get_logger
@@ -20,7 +22,7 @@ class TextProcessor():
         #create chunks
         chunks = self._create_chunks(cleaned_text, document.id)
         
-        self.logger.debug(f"Created {len(chunks)} chunks from document: {document.id}")
+        self.logger.debug(f"Created {len(chunks)} chunks from document")
         return chunks
     
     def _clean_text(self, text: str) -> str:
@@ -34,3 +36,51 @@ class TextProcessor():
         
         return '\n'.join(lines)
     
+    def _create_chunks(self, text: str, document_id) -> List[TextChunk]:
+        """ Create text chunks with overlap """
+        chunks: List[TextChunk] = []
+        words = text.split()
+        
+        if len(words) <= self.chunk_size:
+            # single chunk
+            chunk = TextChunk(
+                document_id = document_id,
+                content = text,
+                start_index = 0,
+                end_index = len(text),
+                chunk_index = 0,
+                token_count=len(words)
+            )
+            chunks.append(chunk)
+        else:
+            #Multiple chunks
+            chunk_index = 0
+            start_word = 0
+            
+            if self.chunk_overlap >= self.chunk_size:
+                raise ValueError(
+                    "chunk_overlap must be smaller than chunk size"
+                )
+                
+            while start_word < len(words):
+                end_word = min(start_word + self.chunk_size, len(words))
+                chunk_words = words[start_word:end_word]
+                chunk_text = ' '.join(chunk_words)
+                
+                chunk = TextChunk(
+                document_id = document_id,
+                content = chunk_text,
+                start_index = start_word,
+                end_index = end_word,
+                chunk_index = chunk_index,
+                token_count=len(chunk_words)
+                )
+                chunks.append(chunk)
+                
+                # Move to next chunk with overlap
+                start_word = end_word - self.chunk_overlap
+                chunk_index += 1
+                
+                if end_word >= len(words):
+                    break
+        return chunks
