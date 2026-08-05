e6c3230a2bd72cae04af96008ee2f790dbda95d8
Roheet <roheetbasu@gmail.com>
2026-08-05 18:58:10 +0545

    adding execute task function

--- SRC/Training_data_bot/tasks/manager.py
+++ SRC/Training_data_bot/tasks/manager.py
@@ -3,7 +3,7 @@ import asyncio
 from typing import Dict, List, Optional
 from uuid import UUID, uuid4
 
-from ..core.models import TaskType, TaskTemplate
+from ..core.models import TaskType, TaskTemplate, TextChunk, TaskResult
 from ..core.exceptions import TaskError, TemplateError
 from ..core.logging import get_logger, LogContext
 from .qa_generation import QAGenerator
@@ -30,6 +30,22 @@ class TaskManager:
     
     async def execute_task(
         self,
-        
-    )
+        task_type: TaskType,
+        input_chunk: Textchunk,
+        client,
+        template_id: Optional[UUID] =None   
+    ) -> TaskResult :
+        """ Execute a task on given text """
+        with LogContext("execute_task", task_type=task_type.value):
+            try:
+                # Get Template
+                if template_id:
+                    template = self.templates.get(template_id)
+                    if not template:
+                        raise TemplateError(f"Template not found: {template_id}")
+                    else:
+                        template = self._get_default_template(task_type.value)
+                        
+                        # get appropriate generetor
+                        generator = self.generators.get(task_type)
         
